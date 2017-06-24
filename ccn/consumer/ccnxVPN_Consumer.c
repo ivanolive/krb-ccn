f237dedb38131d4f81e8dc95f65972e94cb9b0ed
ivanolive <ivanoliv.1@gmail.com>
2017-06-23 18:23:40 -0700

    user crypto material generation added

--- ccn/consumer/ccnxVPN_Consumer.c
+++ ccn/consumer/ccnxVPN_Consumer.c
@@ -1,61 +1,3 @@
-/*
- * Copyright (c) 2016, Xerox Corporation (Xerox) and Palo Alto Research Center, Inc (PARC)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL XEROX OR PARC BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * ################################################################################
- * #
- * # PATENT NOTICE
- * #
- * # This software is distributed under the BSD 2-clause License (see LICENSE
- * # file).  This BSD License does not make any patent claims and as such, does
- * # not act as a patent grant.  The purpose of this section is for each contributor
- * # to define their intentions with respect to intellectual property.
- * #
- * # Each contributor to this source code is encouraged to state their patent
- * # claims and licensing mechanisms for any contributions made. At the end of
- * # this section contributors may each make their own statements.  Contributor's
- * # claims and grants only apply to the pieces (source code, programs, text,
- * # media, etc) that they have contributed directly to this software.
- * #
- * # There is no guarantee that this section is complete, up to date or accurate. It
- * # is up to the contributors to maintain their portion of this section and up to
- * # the user of the software to verify any claims herein.
- * #
- * # Do not remove this header notification.  The contents of this section must be
- * # present in all distributions of the software.  You may only modify your own
- * # intellectual property statements.  Please provide contact information.
- *
- * - Palo Alto Research Center, Inc
- * This software distribution does not grant any rights to patents owned by Palo
- * Alto Research Center, Inc (PARC). Rights to these patents are available via
- * various mechanisms. As of January 2016 PARC has committed to FRAND licensing any
- * intellectual property used by its contributions to this software. You may
- * contact PARC at cipo@parc.com for more information or visit http://www.ccnx.org
- */
-/**
- * @author Nacho Solis, Christopher A. Wood, Palo Alto Research Center (Xerox PARC)
- * @copyright (c) 2016, Xerox Corporation (Xerox) and Palo Alto Research Center, Inc (PARC).  All rights reserved.
- */
 #include <stdio.h>
 #include <getopt.h>
 
@@ -74,6 +16,8 @@
 
 #include "../ccnxVPN_Common.h"
 #include "../ccnxVPN_Stats.h"
+#include "sodium.h"
+
 
 typedef enum {
     CCNxConsumerMode_None = 0,
@@ -82,6 +26,7 @@ typedef enum {
     CCNxConsumerMode_TGTReq,
     CCNxConsumerMode_TGSReq,
     CCNxConsumerMode_KRBServReq,
+    CCNxConsumerMode_KRBConfig,
     CCNxConsumerMode_All
 } CCNxConsumerMode;
 
@@ -213,6 +158,79 @@ _ccnx_CurrentTimeInUs(PARCClock *clock)
     return microseconds;
 }
 
+
+_ccnx_RunTGTReq(CCNxConsumer *client, size_t totalVPNs, uint64_t delayInUs)
+{
+    PARCClock *clock = parcClock_Wallclock();
+
+    printf("Starting TGT request for user <%s>.\n", client->username);
+
+    CCNxPortalFactory *factory = _setupConsumerPortalFactory(client->keystoreName, client->keystorePassword);
+    client->portal = ccnxPortalFactory_CreatePortal(factory, ccnxPortalRTA_Message);
+    ccnxPortalFactory_Release(&factory);
+
+    size_t outstanding = 0;
+    bool checkOustanding = client->numberOfOutstanding > 0;
+
+    for (int pings = 0; pings <= totalVPNs; pings++) {
+        uint64_t nextPacketSendTime = 0;
+        uint64_t currentTimeInUs = 0;
+
+        // Continue to send ping messages until we've reached the capacity
+        if (pings < totalVPNs && (!checkOustanding || (checkOustanding && outstanding < client->numberOfOutstanding))) {
+            CCNxName *name = _ccnx_CreateNextName(client);
+            CCNxInterest *interest = ccnxInterest_CreateSimple(name);
+            CCNxMetaMessage *message = ccnxMetaMessage_CreateFromInterest(interest);
+
+            if (ccnxPortal_Send(client->portal, message, CCNxStackTimeout_Never)) {
+                currentTimeInUs = _ccnx_CurrentTimeInUs(clock);
+                nextPacketSendTime = currentTimeInUs + delayInUs;
+
+                ccnxVPNStats_RecordRequest(client->stats, name, currentTimeInUs);
+            }
+
+            outstanding++;
+            ccnxName_Release(&name);
+        } else {
+            // We're done with pings, so let's wait to see if we have any stragglers
+            currentTimeInUs = _ccnx_CurrentTimeInUs(clock);
+            nextPacketSendTime = currentTimeInUs + client->receiveTimeoutInUs;
+        }
+
+        // Now wait for the responses and record their times
+        uint64_t receiveDelay = nextPacketSendTime - currentTimeInUs;
+        CCNxMetaMessage *response = ccnxPortal_Receive(client->portal, &receiveDelay);
+        while (response != NULL && (!checkOustanding || (checkOustanding && outstanding < client->numberOfOutstanding))) {
+            uint64_t currentTimeInUs = _ccnx_CurrentTimeInUs(clock);
+            if (ccnxMetaMessage_IsContentObject(response)) {
+                CCNxContentObject *contentObject = ccnxMetaMessage_GetContentObject(response);
+
+                CCNxName *responseName = ccnxContentObject_GetName(contentObject);
+                size_t delta = ccnxVPNStats_RecordResponse(client->stats, responseName, currentTimeInUs, response);
+
+                // Only display output if we're in ping mode
+                if (client->mode == CCNxConsumerMode_VPNPong || client->mode == CCNxConsumerMode_TGTReq) {
+                    size_t contentSize = parcBuffer_Remaining(ccnxContentObject_GetPayload(contentObject));
+                    char *nameString = ccnxName_ToString(responseName);
+                    printf("%zu bytes from %s: time=%zu us\n", contentSize, nameString, delta);
+                    parcMemory_Deallocate(&nameString);
+                }
+            }
+            ccnxMetaMessage_Release(&response);
+
+            if (pings < totalVPNs) {
+                receiveDelay = nextPacketSendTime - currentTimeInUs;
+            } else {
+                receiveDelay = client->receiveTimeoutInUs;
+            }
+
+            response = ccnxPortal_Receive(client->portal, &receiveDelay);
+            outstanding--;
+        }
+    }
+}
+
+
 /**
  * Run a single ping test.
  */
@@ -314,17 +332,72 @@ _displayUsage(char *progName)
 
     // Kerberos services///
         printf("\nKerberos services \n");
+        printf("     -n <username> creates a new user on the client host \n");
         printf("     -a <username> User authentication and TGT issuance \n");
         printf("     -t <namespace> Access control and TGS issuance \n");
         printf("     -k <interest name> Access to kerberized service using existent TGS \n\n");
         ///////////////////////
 }
 
+/*
+ * Generates and store cryptographic material for a given user (see Common.h for default storage directory paths)
+ */
+static bool
+ccnx_KRB_addUser(char* userName)
+{
+	unsigned char user_pk[crypto_box_PUBLICKEYBYTES];
+	unsigned char user_sk[crypto_box_SECRETKEYBYTES];
+	crypto_sign_keypair(user_pk, user_sk);
+
+    unsigned char sym_key[crypto_aead_aes256gcm_KEYBYTES+crypto_aead_aes256gcm_NPUBBYTES];
+	randombytes_buf(sym_key, sizeof sym_key);
+
+	char* fileName = (char*) malloc(strlen(userName) + strlen(userPrvDir) + strlen("-prv") + 1);
+	strcpy(fileName, userPrvDir);
+	strcat(fileName, userName);
+	strcat(fileName, "-prv");
+
+	if (!fopen(fileName,"r")) {
+
+		// Writing secret key to default location////////
+		FILE* user_keys = fopen(fileName,"w");
+		fwrite(user_sk, sizeof(char), crypto_box_SECRETKEYBYTES, user_keys);
+		fclose(user_keys);
+		////////////////////////////////////////////////
+
+		// Writing public key to default location////////
+		strcpy(fileName, userPrvDir);
+		strcat(fileName, userName);
+		strcat(fileName, "-pub");
+		user_keys = fopen(fileName,"w");
+		fwrite(user_pk, sizeof(char), crypto_box_PUBLICKEYBYTES, user_keys);
+		fclose(user_keys);
+		/////////////////////////////////////////////////
+
+		// Writing symmetric to default location//////////
+		strcpy(fileName, userPrvDir);
+		strcat(fileName, userName);
+		strcat(fileName, "-sym");
+		user_keys = fopen(fileName,"w");
+		fwrite(sym_key,sizeof(char),crypto_aead_aes256gcm_KEYBYTES+crypto_aead_aes256gcm_NPUBBYTES,user_keys);
+		fclose(user_keys);
+		/////////////////////////////////////////////////
+
+		printf("User <%s> was created successfully.\n", userName);
+	} else {
+		printf("Username <%s> is already in use.\n", userName);
+	}
+	free(fileName);
+	return false;	//close software after adding the user
+}
+
+
 static bool
 _ccnx_KRB_ParseCommandline(CCNxConsumer *client, int argc, char *argv[argc])
 {
 
 	static struct option longopts[] = {
+		{ "KRB adduser",		required_argument,       NULL, 'n' },
     	{ "TGT",		required_argument,       NULL, 'a' },
     	{ "TGS",		required_argument,       NULL, 't' },
     	{ "KRB_SERV",   required_argument,       NULL, 'k' },
@@ -342,7 +415,7 @@ _ccnx_KRB_ParseCommandline(CCNxConsumer *client, int argc, char *argv[argc])
     client->payloadSize = ccnx_DefaultPayloadSize;
 
     int c;
-    while ((c = getopt_long(argc, argv, "a:t:k:p:i:h:f:c:s:l:o:", longopts, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, "n:a:t:k:p:i:h:f:c:s:l:o:", longopts, NULL)) != -1) {
         switch (c) {
         	case 'a':
         		printf("TGT User Authentication.\n");
@@ -354,6 +427,8 @@ _ccnx_KRB_ParseCommandline(CCNxConsumer *client, int argc, char *argv[argc])
         		client->mode = CCNxConsumerMode_TGTReq;
         		//XXX: End of TGT Req network options
 
+        		printf("adding user\n");
+        		ccnx_KRB_addUser(optarg);
         		//Reading username
         		client->username = malloc(strlen(optarg) + 1);
                 strcpy(client->username, optarg);
@@ -368,6 +443,10 @@ _ccnx_KRB_ParseCommandline(CCNxConsumer *client, int argc, char *argv[argc])
         		break;
         	case 't':
         	    printf("TGS Service Access Control Verification.\n");
+        	    break;
+        	case 'n':
+        		ccnx_KRB_addUser(optarg);
+        		client->mode = CCNxConsumerMode_KRBConfig;
         	    break;
         	case 'k':
         	    printf("Kerberized service interest issuance.\n");
@@ -414,9 +493,12 @@ _ccnx_KRB_ParseCommandline(CCNxConsumer *client, int argc, char *argv[argc])
         }
     }
 
-    //end:
     if (client->mode == CCNxConsumerMode_None) {
         _displayUsage(argv[0]);
+        return false;
+    }
+
+    if (client->mode == CCNxConsumerMode_KRBConfig) {
         return false;
     }
 
@@ -458,8 +540,7 @@ _ccnx_RunKerberizedClient(CCNxConsumer *client)
             _ccnx_DisplayStatistics(client);
             break;
         case CCNxConsumerMode_TGTReq:
-            //TODO: check this
-            _ccnx_RunVPN(client, client->count, client->intervalInMs);
+            _ccnx_RunTGTReq(client, client->count, client->intervalInMs);
             _ccnx_DisplayStatistics(client);
             break;
 
@@ -478,11 +559,17 @@ main(int argc, char *argv[argc])
 
 	parcSecurity_Init();
 
+	int check = sodium_init();
+	if (check) {
+		printf("Crypto lib Sodium not available.\n");
+	}
+
     CCNxConsumer *client = ccnx_Create();
 
     printf("KBR-CCN: Preparing to Run Consumer...\n");
 
     bool runKRB = _ccnx_KRB_ParseCommandline(client, argc, argv);
+
     if (runKRB) {
         _ccnx_RunKerberizedClient(client);
     }
